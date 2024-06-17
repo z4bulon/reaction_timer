32bab16f77e750a21b4c993607149c33c29f3385
z4bulon <z8bulon@gmail.com>
2024-06-17 20:37:22 +0500

    Updated App.vue, added some components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer Game</h1>
+  <button @click="start" :disabled="isPlaying"> Play </button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"></Results>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Results,
+    Block
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(time){
+      this.score = time
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -22,5 +46,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding:8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,42 @@
+<template>
+  <h2>Your reaction time is {{score}} ms</h2>
+  <p v-if="rank === 'Rapid Reflexes'" class="rank rapid">{{rank}}</p>
+  <p v-else-if="rank === 'Snail pace'" class="rank snail">{{rank}}</p>
+  <p v-else class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props:['score'],
+  data(){
+    return{
+      rank:null,
+    }
+  },
+  mounted() {
+    if(this.score < 300){
+      this.rank = 'Rapid Reflexes'
+    }
+    else if(this.score > 400){
+      this.rank = 'Snail pace'
+    }
+    else{
+      this.rank = 'Average'
+    }
+  }
+}
+</script>
+
+<style>
+  .rank{
+    font-size:1.4em;
+    color: #2c3e50;
+    font-weight: bold;
+  }
+  .snail{
+    color:crimson;
+  }
+  .rapid{
+    color: #0faf87;
+  }
+</style>
